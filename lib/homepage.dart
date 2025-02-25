748f13686439d1d5184efae0789413d6525efd54
MatiasCaamal <matiascaamal@hotmail.com>
2025-02-24 21:07:48 -0600

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
